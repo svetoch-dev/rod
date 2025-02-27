4468d1dd09a76f1321efa5fee09cf12abd5b31a1
Gramovich Alexander <gramovich.alex@outlook.com>
2025-02-27 18:58:45 +0300

    Init scripts

---
+++ scripts/init/images/build/build.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+
+WORKSPACE_FOLDER = os.getenv("BUILD_WORKSPACE_DIRECTORY")
+
+
+def process_results(result):
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        error = f"Bazel query failed with return code: {result.returncode}\n"
+        error += "Error:\n"
+        error += result.stderr
+
+        raise BaseException(error)
+
+
+def build_images():
+    """
+    Build and push custom images to container registries
+
+    This is done
+    1. by executing bazel command and query all targets starting with `push_` in all subpackages of deps/images
+    2. executing all those target via `bazel run`
+    """
+    os.chdir(WORKSPACE_FOLDER)
+    query = ["bazel", "query", 'attr(name, "^push_.*$", "//deps/images/...")']
+    result = subprocess.run(query, capture_output=True, text=True)
+    output = process_results(result)
+    output = output.strip("\n")
+    for run_command in output.split("\n"):
+        run_command = ["bazel", "run", run_command]
+        run_command_str = " ".join(run_command)
+        print(f"Running: {run_command_str}")
+        result = subprocess.Popen(run_command, stdout=subprocess.PIPE, text=True)
+        for line in result.stdout:
+            print(line.strip())
+
+        result.wait()
+        if result.returncode != 0:
+            print("Command failed with return code:", result.returncode)
+
+
+if __name__ == "__main__":
+    build_images()
---
+++ scripts/init/images/prepare/prepare.py
@@ -0,0 +1,41 @@
+import click
+import os
+import json
+from config import DOCKER_CONFIG_FILE
+
+
+@click.command()
+@click.argument("registries", required=True, type=click.STRING)
+@click.argument("creds_helper_name", required=True, type=click.STRING)
+def image_prepare(registries, creds_helper_name):
+    """
+    1. Downloads docker-credential-gcr
+    2. creates a credHelpers section in ~/.docker/config.json based on registries
+
+    Args:
+        registries(str): comma separated list of container registries
+        creds_helper_name(str): credential helper name (eg gar, gcloud etc)
+    """
+    configs = {"auths": {}, "credHelpers": {}}
+    try:
+        with open(DOCKER_CONFIG_FILE, "r") as f:
+            configs = json.load(f)
+    except json.JSONDecodeError:
+        print(
+            f"Error: The file '{DOCKER_CONFIG_FILE}' contains invalid JSON. Or is empty"
+        )
+        print("Using default configs")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    registries = registries.split(",")
+    for registry in registries:
+        registry = registry.split("/")[0]
+        configs["credHelpers"][registry] = creds_helper_name
+
+    with open(DOCKER_CONFIG_FILE, "w") as f:
+        json.dump(configs, f, indent=4)
+
+
+if __name__ == "__main__":
+    image_prepare()
