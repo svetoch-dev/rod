3a25cd165ac6d324acecddb3d082961cd7b3e932
Gramovich Alexander <gramovich.alex@outlook.com>
2025-03-21 15:56:02 +0300

    Rename github tfvars attr to ci in order to make it more generic

--- terraform/environments/internal/github/tf_locals.tf
+++ terraform/environments/internal/github/tf_locals.tf
@@ -8,26 +8,11 @@ locals {
         }
       }
     },
-    {
-      for name, obj in var.envs :
-      "gcp-${obj.short_name}" => {
-        config = {
-          bucket = "${var.company.name}-tf-state"
-          prefix = "${obj.name}/gcp"
-        }
-      }
-    }
   )
 
   remote_state = merge(
     {
       secrets = data.terraform_remote_state.remote_state["secrets"].outputs.secrets,
     },
-    {
-      for name, obj in var.envs :
-      "gcp-${obj.short_name}" => {
-        service_accounts = data.terraform_remote_state.remote_state["gcp-${obj.short_name}"].outputs.service_accounts
-      }
-    }
   )
 }
