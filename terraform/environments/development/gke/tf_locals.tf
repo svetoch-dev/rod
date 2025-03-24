571b76ee8f49d1ac56d18373fd4cf457d258b149
Gramovich Alexander <gramovich.alex@outlook.com>
2025-03-24 21:36:07 +0300

    terraform/gke: use local.env.kubernetes.location to set location

--- terraform/environments/development/gke/tf_locals.tf
+++ terraform/environments/development/gke/tf_locals.tf
@@ -1,7 +1,7 @@
 locals {
   gke_clusters = {
     this = {
-      location = "${local.env.cloud.region}-a"
+      location = local.env.kubernetes.location
       project  = local.env.cloud.id
       name     = local.env.short_name
       enabled  = true
