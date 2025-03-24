fc8094077c2dff08643338e46f8dbae46e2e74e4
Gramovich Alexander <gramovich.alex@outlook.com>
2025-03-24 18:10:00 +0300

    Remote remote secrets from github

--- terraform/environments/internal/github/tf_locals.tf
+++ terraform/environments/internal/github/tf_locals.tf
@@ -1,18 +1,5 @@
 locals {
-  remote_state_config = merge(
-    {
-      secrets = {
-        config = {
-          bucket = "${var.company.name}-tf-state"
-          prefix = "${local.env.name}/secrets"
-        }
-      }
-    },
-  )
+  remote_state_config = {}
 
-  remote_state = merge(
-    {
-      secrets = data.terraform_remote_state.remote_state["secrets"].outputs.secrets,
-    },
-  )
+  remote_state = {}
 }
