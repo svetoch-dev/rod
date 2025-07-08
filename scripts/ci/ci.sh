8e96241f11922b476c899a27939546de2f03ecfd
Gramovich Alexander <gramovich.alex@outlook.com>
2025-07-08 17:52:34 +0300

    use common bazel-lib

--- scripts/ci/ci.sh
+++ scripts/ci/ci.sh
@@ -27,7 +27,7 @@ prepare_step() {
     curl -fsSL $DOCKER_CRED_URL | tar xz docker-credential-gcr && chmod +x docker-credential-gcr 
     mkdir ~/tools/ && mv ./docker-credential-gcr ~/tools/ 
 
-    bazel run //scripts/init/images/prepare
+    bazel run @svetoch_bazel_lib//scripts/init/images/prepare
 }
 
 run_step() {
