a65d0bd29e6d67dc24f5346b4bc5aa27d30323f5
Gramovich Alexander <gramovich.alex@outlook.com>
2026-03-11 18:00:08 +0300

    move e2e tests under tests folder + refactor poststeps to set needed attributes via tfvars models

---
+++ tests/e2e/prepare/prepare.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from libs.py.tf.tfvars import tfvars
+from libs.py.settings import bazel_settings
+
+def prepare():
+    tf_vars = tfvars()
+    tf_vars.repo.name = "rod-e2e"
+    for env_name, env_obj in tf_vars.envs.items():
+        env_obj.cloud.buckets.deletion_protection = False
+        env_obj.kubernetes.deletion_protection = False
+        env_obj.initial_start = True
+        #bazel does not support null in json
+        if not env_obj.cloud.folder_id:
+            env_obj.cloud.folder_id = ""
+
+    Path(bazel_settings.tfvars_file).write_text(
+        tf_vars.model_dump_json(indent=2),
+        encoding="utf-8",
+    )
+
+
+if __name__ == "__main__":
+    prepare()
