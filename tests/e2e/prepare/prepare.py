ab307ad9ad7c601abfb024206fab5d81dec4fe5e
Gramovich Alexander <gramovich.alex@outlook.com>
2026-03-11 22:03:57 +0300

    tests/e2e/prepare adapt to template env name

--- tests/e2e/prepare/prepare.py
+++ tests/e2e/prepare/prepare.py
@@ -6,6 +6,9 @@ from libs.py.settings import bazel_settings
 def prepare():
     tf_vars = tfvars()
     tf_vars.repo.name = "rod-e2e"
+    envs = tf_vars.envs
+    envs["production"] = envs.pop("template")
+
     for env_name, env_obj in tf_vars.envs.items():
         env_obj.cloud.buckets.deletion_protection = False
         env_obj.kubernetes.deletion_protection = False
@@ -13,6 +16,11 @@ def prepare():
         # bazel does not support null in json
         if not env_obj.cloud.folder_id:
             env_obj.cloud.folder_id = ""
+
+        if env_obj.short_name == "tpl":
+            env_obj.short_name = "prd"
+            env_obj.name = "production"
+
 
     Path(bazel_settings.tfvars_file).write_text(
         tf_vars.model_dump_json(indent=2),
