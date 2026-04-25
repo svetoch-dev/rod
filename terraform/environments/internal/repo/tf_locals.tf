92201c8188af019fd3e4e1c60a900c550e9a087a
Dzmitry.St <125599832+dmitry231187@users.noreply.github.com>
2026-04-25 12:48:11 +0300

    Ci vars (#62)
    * update CI
    
    * use bazelisk var in ci
    
    * fix terraform.tfvars.json
    
    * lint
    
    * update svetoch_bazel_lib
    
    * use ../../../../file instead genrule

--- terraform/environments/internal/repo/tf_locals.tf
+++ terraform/environments/internal/repo/tf_locals.tf
@@ -1,4 +1,6 @@
 locals {
+  bazelisk_img_version = var.ci.bazelisk_img_version == "" ? trimspace(file("../../../../deps/images/bazelisk/image_tag.txt")) : var.ci.bazelisk_img_version
+
   remote_state_config = {
   }
 
