e398d914e6d178df40eee603c31b5540095ea62a
Gramovich Alexander <gramovich.alex@outlook.com>
2026-04-30 22:04:05 +0300

    lint fix + terraform/environments/internal/secrets/: use env.type

--- terraform/environments/internal/secrets/main.tf.tpl
+++ terraform/environments/internal/secrets/main.tf.tpl
@@ -39,6 +39,6 @@ module "secrets" {
   overrides       = local.overrides
   argocd_clusters = {
     for cluster_name, cluster_obj in local.remote_state.k8s_clusters : cluster_name => cluster_obj
-    if cluster_name != "int"
+    if clou!= "int"
   }
 }
--- tests/e2e/prepare/prepare.py
+++ tests/e2e/prepare/prepare.py
@@ -4,33 +4,31 @@ from pathlib import Path
 from rod.libs.py.tf.tfvars import tfvars, TfVars, TfBackend, Dns, Registry, Location
 from rod.libs.py.settings import bazel_settings
 
+
 def prepare_gcp(tfvars: TfVars):
     envs = tfvars.envs
     tf_backend = {
-      "type": "gcs",
-      "configs": {
-        "bucket": "{company.name}-tf-state",
-        "prefix": "{env.name}/{tf_backend.state_name}"
-      }
+        "type": "gcs",
+        "configs": {
+            "bucket": "{company.name}-tf-state",
+            "prefix": "{env.name}/{tf_backend.state_name}",
+        },
     }
     registry = {
-      "type": "gar",
-      "url": "{env.cloud.location.region}-docker.pkg.dev/{env.cloud.id}/containers"
+        "type": "gar",
+        "url": "{env.cloud.location.region}-docker.pkg.dev/{env.cloud.id}/containers",
     }
-    dns = {
-      "domain": "{env.short_name}.{company.domain}.",
-      "type": "gcp"
-    }
-    location =  {
-      "region": "europe-west2",
-      "default_zone": "europe-west2-c",
-      "multi_region": "EU"
+    dns = {"domain": "{env.short_name}.{company.domain}.", "type": "gcp"}
+    location = {
+        "region": "europe-west2",
+        "default_zone": "europe-west2-c",
+        "multi_region": "EU",
     }
     for env_name, env_obj in tfvars.envs.items():
         env_obj.cloud.name = "gcp"
         env_obj.tf_backend = TfBackend(**tf_backend)
-        env_obj.registry   = Registry(**registry)
-        env_obj.dns  = Dns(**dns)
+        env_obj.registry = Registry(**registry)
+        env_obj.dns = Dns(**dns)
         env_obj.cloud.location = Location(**location)
         env_obj.cloud.folder_id = ""
         env_obj.kubernetes.node_locations = ["europe-west2-a"]
@@ -39,39 +37,34 @@ def prepare_gcp(tfvars: TfVars):
         if env_obj.type == "internal":
             env_obj.cloud.id = "rod-internal"
 
+
 def prepare_yc(tfvars: TfVars):
     envs = tfvars.envs
     tf_backend = {
         "type": "s3",
         "configs": {
-          "bucket": "{company.name}-tf-state",
-          "use_lockfile": "true",
-          "region": "{env.cloud.region}",
-          "key": "{env.name}/{tf_backend.state_name}/default.tfstate",
-          "skip_region_validation": "true",
-          "skip_credentials_validation": "true",
-          "skip_requesting_account_id": "true",
-          "skip_s3_checksum": "true"
-        }
+            "bucket": "{company.name}-tf-state",
+            "use_lockfile": "true",
+            "region": "{env.cloud.region}",
+            "key": "{env.name}/{tf_backend.state_name}/default.tfstate",
+            "skip_region_validation": "true",
+            "skip_credentials_validation": "true",
+            "skip_requesting_account_id": "true",
+            "skip_s3_checksum": "true",
+        },
     }
-    registry = {
-      "type": "ycr",
-      "url": ""
-    }
-    dns = {
-      "domain": "{env.short_name}.{company.domain}.",
-      "type": "yc"
-    }
-    location =  {
-      "region": "ru-central1",
-      "default_zone": "ru-central1-a",
-      "multi_region": ""
+    registry = {"type": "ycr", "url": ""}
+    dns = {"domain": "{env.short_name}.{company.domain}.", "type": "yc"}
+    location = {
+        "region": "ru-central1",
+        "default_zone": "ru-central1-a",
+        "multi_region": "",
     }
     for env_name, env_obj in tfvars.envs.items():
         env_obj.cloud.name = "gcp"
         env_obj.tf_backend = TfBackend(**tf_backend)
-        env_obj.registry   = Registry(**registry)
-        env_obj.dns  = Dns(**dns)
+        env_obj.registry = Registry(**registry)
+        env_obj.dns = Dns(**dns)
         env_obj.cloud.location = Location(**location)
         env_obj.kubernetes.node_locations = ["ru-central1-d"]
         if env_obj.type == "product":
@@ -82,9 +75,10 @@ def prepare_yc(tfvars: TfVars):
             env_obj.cloud.folder_id = "b1gfu8oas3od212hedtu"
 
 
-
 @click.command()
-@click.argument("cloud", required=False, default="gcp", type=click.Choice(["gcp", "yc"]))
+@click.argument(
+    "cloud", required=False, default="gcp", type=click.Choice(["gcp", "yc"])
+)
 def prepare(cloud: str):
     tf_vars = tfvars()
     tf_vars.repo.name = "rod-e2e"
