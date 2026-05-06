ce5e68301368d83aa19bd1cba624250b91cc8ec9
dmitry231187 <leshiy74@gmail.com>
2026-05-06 07:21:56 +0000

    simplified tf_locals

--- terraform/environments/internal/ci/tf_locals.tf
+++ terraform/environments/internal/ci/tf_locals.tf
@@ -19,14 +19,9 @@ locals {
     [
       for app_name, app_data in env_obj.apps :
       {
-        ci_name    = "${app_data.name == "" ? app_name : app_data.name}_${env_obj.short_name}"
-        repo_name  = app_data.ci.repo_name
-        repo_group = app_data.ci.repo_group
-        cd_branch  = app_data.ci.cd_branch
-        cd_file    = app_data.ci.cd_file
-        cd_path    = app_data.ci.cd_path
-        vars       = app_data.ci.vars
-        secrets    = app_data.ci.secrets
+        ci_name        = "${app_data.name == "" ? app_name : app_data.name}_${env_obj.short_name}"
+        env_short_name = env_obj.short_name
+        ci_data        = app_data.ci
       }
       if app_data.ci != null && length(app_data.ci) > 0
     ]
