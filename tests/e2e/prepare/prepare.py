14b8c70fb08e2aa510ac36e712a07a6962f82e14
Gramovich Alexander <gramovich.alex@outlook.com>
2026-05-20 16:42:32 +0300

    lint fix

--- tests/e2e/prepare/prepare.py
+++ tests/e2e/prepare/prepare.py
@@ -103,8 +103,8 @@ def prepare(cloud: str):
 
     for env_name, env_obj in tf_vars.envs.items():
         for app_name, app_obj in env_obj.apps.items():
-          app_obj.repo = None
-          app_obj.cd = None
+            app_obj.repo = None
+            app_obj.cd = None
         env_obj.cloud.buckets.deletion_protection = False
         env_obj.kubernetes.deletion_protection = False
         env_obj.initial_start = True
